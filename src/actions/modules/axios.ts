c4649c9369d4e1af30d16adff3ccadf89576a8d3
C12H22O12 <director20844@gmail.com>
2023-05-14 17:40:32 +0900

    feat : infinite scroll

--- src/actions/modules/axios.ts
+++ src/actions/modules/axios.ts
@@ -1,10 +1,8 @@
 import { url } from "@src/constant/variable";
 import axios from "axios";
 
-const API = axios.create({ baseURL: url });
-
 export const getAsync = (additionalUrl: string) => {
-  return API.get(additionalUrl, { responseType: "json" })
+  return axios.get(`${url}${additionalUrl}`, { responseType: "json" })
     .then((res) => {
       return { isSuccess: true, result: res.data.response };
     })
--- src/present/pages/Home/Home.tsx
+++ src/present/pages/Home/Home.tsx
@@ -4,26 +4,43 @@ import { getAsync } from "@src/actions/modules/axios";
 import Article from "@src/present/component/Article/Article";
 import { ArticleType } from "@src/types/Article";
 import Loading from "@src/present/layout/Loading/Loading";
+import useInfinite from "@src/actions/hooks/useInfinite";
+import { url } from "@src/constant/variable";
 
 function Home() {
   const [articleList, setArticleList] = useState<Array<ArticleType | any>>([]);
+  const [page, setPage] = useState<number>(1);
+  const [moreData, setMoreDate] = useState<boolean>(true);
 
-  useEffect(() => {
-    getAsync("").then((res) => {
+  const getData = async () => {
+    await getAsync(`&page=${page}`).then((res) => {
       if (res.isSuccess) {
-        setArticleList([...res.result.docs]);
+        setArticleList((prev) => prev.concat(...res.result.docs));
+        setPage((prev) => {
+          const next = prev + 1;
+
+          return (next >= 5) ? 1 : next;
+        });
         console.log(res.result);
+      } else {
+        setMoreDate(false);
       }
     });
-  }, []);
+  };
+
+  const target = useInfinite(async (entry, observer) => {
+    await getData();
+  });
 
   const articles = articleList.map((elem, idx) => {
     return <Article key={idx} item={elem} />;
   });
 
-//   return <Loading />;
-
-    return <div className="ArticleLayout">{articles}</div>;
+  return (
+    <div className="ArticleLayout">
+      {articles} {moreData ? <div ref={target} /> : null}
+    </div>
+  );
 }
 
 export default memo(Home);
